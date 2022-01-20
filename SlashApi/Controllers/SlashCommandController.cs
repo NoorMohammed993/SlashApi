b2409cadd104c3edda7a39802d25feea74805b57
NoorMohammed993 <noormohammed401401@gmail.com>
2022-01-20 20:20:52 +0300

    Add project files.

---
+++ SlashApi/Controllers/SlashCommandController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace SlashApi.Controllers
+{
+    public class SlashCommandController : Controller
+    {
+        [Route("/api/slack/slashcommand")]
+        [HttpPost]
+        [Produces("application/json")]
+        public string HandleCommand(SlashCommandPayload slashCommandPayload)
+        {
+            Ping sender = new();
+            PingReply reply = sender.Send("8.8.8.8", 1000);
+
+            return $"Ping to 8.8.8.8 sent, Ping {reply.Status} reply Message";
+        }
+    }
+}
